6ff5bfcc3291317b97aea99e16c47838dddcbcb0
Bruno Laine <bruno.laine94@gmail.com>
2022-04-10 11:49:31 +0200

    feat(frontend): add eslint config

--- perftest-front/src/components/RessourcesTimeDiagram.js
+++ perftest-front/src/components/RessourcesTimeDiagram.js
@@ -1,46 +1,48 @@
 import React from 'react';
-import { Chart } from "react-google-charts";
+import { Chart } from 'react-google-charts';
 
-export default function RessourcesTimeDiagram({data}) {
-    //get the list of ressources
-    let ressourcesUrls = [];
-    data.forEach(({ressources})=>{
-        ressources.forEach(({url}) => {
-            if(!ressourcesUrls.includes(url)){
-                ressourcesUrls.push(url);
-            }
-        });
+export default function RessourcesTimeDiagram({ data: metrics = [] }) {
+  // get the list of ressources
+  const ressourcesUrls = [];
+  try {
+    metrics.forEach(({ ressources = [] }) => {
+      ressources.forEach(({ url }) => {
+        if (!ressourcesUrls.includes(url)) {
+          ressourcesUrls.push(url);
+        }
+      });
     });
-    //for each datapoint, get the loading time of each ressource
-    let dataHeader = ["timestamp"].concat(ressourcesUrls);
-    let graphData = [dataHeader];
-    data.forEach(({timestamp, ressources}) => {
-        const row = [new Date(timestamp)].concat(ressourcesUrls.map((url, index) => {
-            const foundRessource = ressources.find((ressource) => ressource.url === url);
-            if(foundRessource){
-                return foundRessource.endTime - foundRessource.startTime;
-            }
-            else {
-                return 0
-            };
-        }));
-        graphData.push(row);
-    });
-    const options = {
-        title: "Loading time per ressource",
-        curveType: "function",
-        legend: { position: "right" },
-        hAxis: {
-          format: 'M/d/yy',
-        },
-    };
-    return(
-        <Chart
-          chartType="LineChart"
-          width="100%"
-          height="400px"
-          data={graphData}
-          options={options}
-        />
-    );
+  } catch (error) {
+    console.error(error);
+  }
+  // for each datapoint, get the loading time of each ressource
+  const dataHeader = ['timestamp'].concat(ressourcesUrls);
+  const graphData = [dataHeader];
+  metrics.forEach(({ timestamp, ressources }) => {
+    const row = [new Date(timestamp)].concat(ressourcesUrls.map((url) => {
+      const foundRessource = ressources.find((ressource) => ressource.url === url);
+      if (foundRessource) {
+        return foundRessource.endTime - foundRessource.startTime;
+      }
+
+      return 0;
+    }));
+    graphData.push(row);
+  });
+  const options = {
+    title: 'Loading time per ressource',
+    curveType: 'function',
+    legend: { position: 'right' },
+    vAxis: { title: 'Time in ms' },
+    hAxis: { title: 'Time of sample' },
+  };
+  return (
+    <Chart
+      chartType="LineChart"
+      width="100%"
+      height="400px"
+      data={graphData}
+      options={options}
+    />
+  );
 }
